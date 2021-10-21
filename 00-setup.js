2bdae012310614910605f1bbc60178694e1416af
AWilburn228 <90441512+AWilburn228@users.noreply.github.com>
2021-10-21 17:11:49 -0400

    Add files via upload

---
+++ 00-setup.js
@@ -0,0 +1,13 @@
+function hello() { 
+    console.log("Hello World!");
+} 
+ 
+let x; // declares a variable named x  
+ 
+x = 20 
+ 
+let numberOfCookies = 20; 
+ 
+let cookiesEaten = 3; 
+ 
+let newNumber = 17 *numberOfCookies
